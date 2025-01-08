528d352c107cce908b67e83f2ef1f5451ff535b8
Michael Woodard <mr.m.woodard@gmail.com>
2025-01-08 10:27:35 -0500

    updated

--- froyo.js
+++ froyo.js
@@ -1,7 +1,7 @@
 const froyoFlavors = {};
 const orders = prompt(
   "Enter the flavor of yogurt you'd like to order:",
-  "vanilla,vanilla,vanilla,strawberry,coffee,coffee"
+  "vanilla,vanilla,vanilla,strawberry,coffee,coffee,taro,matcha,matcha,bacon,taro"
 );
 const splitOrder = orders.split(",");
 froyoFlavors.orders = splitOrder;
@@ -9,7 +9,7 @@ console.log(froyoFlavors.orders);
 
 const countOrder = {};
 
-for (order in froyoFlavors.orders) {
+for (order in splitOrder) {
   if (!countOrder[splitOrder[order]]) {
     countOrder[splitOrder[order]] = 1;
   } else {
